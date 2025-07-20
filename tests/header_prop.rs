60442939523a5b19f5ec9027c9c180f906202c5a
coldshalamov <93robingattis@gmail.com>
2025-07-20 09:12:50 -0400

    Add fuzzing and coverage CI

--- tests/header_prop.rs
+++ tests/header_prop.rs
@@ -1,20 +1,23 @@
 //! See [Kolyma Spec](../kolyma.pdf) - 2025-07-20 - commit c48b123cf3a8761a15713b9bf18697061ab23976
-use quickcheck::quickcheck;
+use proptest::prelude::*;
 use telomere::{decode_header, encode_header, Header};
 
-quickcheck! {
-    fn header_roundtrip(arity: u8, is_literal: bool) -> bool {
+proptest! {
+    #[test]
+    fn header_roundtrip(arity in 0u8..=255, is_literal in proptest::bool::ANY) {
         if is_literal {
             let h = Header::Literal;
             let enc = encode_header(&h).unwrap();
-            return matches!(decode_header(&enc), Ok((d, _)) if d == h);
+            prop_assert!(matches!(decode_header(&enc), Ok((d, _)) if d == h));
+        } else {
+            let a = (arity % 5) + 1;
+            if a == 2 {
+                prop_assert!(encode_header(&Header::Arity(a)).is_err());
+            } else {
+                let h = Header::Arity(a);
+                let enc = encode_header(&h).unwrap();
+                prop_assert!(matches!(decode_header(&enc), Ok((d, _)) if d == h));
+            }
         }
-        let a = (arity % 5) + 1;
-        if a == 2 {
-            return encode_header(&Header::Arity(a)).is_err();
-        }
-        let h = Header::Arity(a);
-        let enc = encode_header(&h).unwrap();
-        matches!(decode_header(&enc), Ok((d, _)) if d == h)
     }
 }
